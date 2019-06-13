b9de0d25239147cb435d4121a44bc3fcdbc1f187
mr-moonpie <p_rackwitz@outlook.com>
2019-06-13 17:13:31 +0200

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 echo '==================='
+echo $ACTION
+echo $COPADO_ENDPOINT_HOSTNAME
+echo $C1P_APIKEY
+echo $C1P_ORGCREDENTIALID_CLIENT
+echo $PUBLIC_REPOSITORY_URL
+echo $C1P_ORGCREDENTIALID_CLIENT_ENDPOINT
+echo $C1P_ORGCREDENTIALID_CLIENT_AUTH_HEADER
+echo '==================='
 notify_status "Doing%20something" "10"
 
 echo "Sleeping for 2 seconds..."
