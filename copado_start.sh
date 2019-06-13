881358599dbe80ab8af2bc7006a17a1cae9b6890
mr-moonpie <p_rackwitz@outlook.com>
2019-06-13 10:38:05 +0200

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -4,21 +4,21 @@ echo '==================='
 notify_status "10" "Doing%20something"
 
 echo "Sleeping for 2 seconds..."
-sleep 2
-notify_status "20" "Starting%20script%2020"
+sleep 1
+notify_status "Starting%20script%2020" "20"
 
 echo "Sleeping for 2 seconds..."
-sleep 2
-notify_status "40" "Working%20to%2040%"
+sleep 1
+notify_status "Working%20to%2040%" "40"
 
 echo "Sleeping for 2 seconds..."
-sleep 2
+sleep 1
 notify_status "60" "Working%20to%2060%"
 
 echo "Sleeping for 2 seconds..."
-sleep 2
+sleep 1
 notify_status "80" "Working%20to%2080%"
 
 echo "Sleeping for 2 seconds..."
-sleep 2
+sleep 1
 notify_status "100" "Finishing%20stuff%20up"
