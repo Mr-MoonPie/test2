82e6b688c28c93c892970d06794e5deddd98bcb7
mr-moonpie <p_rackwitz@outlook.com>
2019-06-15 00:47:36 +0200

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -11,22 +11,21 @@ echo $C1P_ORGCREDENTIALID_CLIENT_AUTH_HEADER
 echo '==================='
 notify_status "Doing%20something" "10"
 
-echo "Sleeping for 2 seconds..."
-sleep 1
-notify_status "Starting%20script%2020" "20"
+#echo "Sleeping for 2 seconds..."
+#sleep 1
+#notify_status "Starting%20script%2020" "20"
 
-echo "Sleeping for 2 seconds..."
-sleep 1
-notify_status "Working%20to%2040%" "40"
+#echo "Sleeping for 2 seconds..."
+#sleep 1
+#notify_status "Working%20to%2040%" "40"
 
-echo "Sleeping for 2 seconds..."
-sleep 1
-notify_status "Working%20to%2060%" "60"
+#echo "Sleeping for 2 seconds..."
+#sleep 1
+#notify_status "Working%20to%2060%" "60"
 
-echo "Sleeping for 2 seconds..."
-sleep 1
-notify_status "Working%20to%2080%" "80"
+#echo "Sleeping for 2 seconds..."
+#sleep 1
+#notify_status "Working%20to%2080%" "80"
 
-echo "Sleeping for 2 seconds..."
 sleep 1
 notify_status "Finishing%20stuff%20up" "100"
